3ef49287405535920b81b9938aaf2523098b0902
tnkHub <2604224@students.wits.ac.za>
2025-05-21 15:58:48 +0200

    Initial

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-2.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"5cf485ee","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"2537550@students.wits.ac.za","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-5cf485ee', 2 );
+});
+ });
